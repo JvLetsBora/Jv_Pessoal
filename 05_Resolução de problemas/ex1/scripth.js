b62577b689031a430e9faf5aef187be13f55ac34
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2022-05-25 00:35:18 -0300

    CalculadoraCssOn

---
+++ 05_Resolução de problemas/ex1/scripth.js
@@ -0,0 +1,28 @@
+var x=0;
+var y=0;
+var res =0;
+
+
+function resultado(){
+}
+
+function operacao(a){
+
+    if(a == "+"){
+  
+    }
+    else if (a == "-"){
+
+    }
+    else if (a == "*"){
+
+    }
+    else if (a == "%"){
+
+    }
+    else{res = x/y}
+    
+
+
+
+}

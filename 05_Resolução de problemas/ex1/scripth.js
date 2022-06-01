88be7a48137c7a4bee3c4184846e8127825e5efb
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2022-06-01 09:09:41 -0300

    .

--- 05_Resolução de problemas/ex1/scripth.js
+++ 05_Resolução de problemas/ex1/scripth.js
@@ -1,28 +1,46 @@
-var x=0;
+var x="";
 var y=0;
 var res =0;
+var porta = false;
+var v = false;
+var opera = "";
 
+function numero(a){
+    if(v == false){
+        document.getElementById("res").value = "";
+        v = true;
+    }
+    if( porta == false){
+         x = a;
+         document.getElementById("res").value += x;
+         
+    }else{ y = a; document.getElementById("res").value += y;}
+}
 
 function resultado(){
+    if(opera == "+"){
+        res = parseInt(x)+parseInt(y)  
+    }
+    else if (opera == "-"){
+        
+        res = parseInt(x)-parseInt(y);
+    }
+    else if (opera == "*"){
+        res = parseInt(x)*parseInt(y);
+    }
+    else if (opera == "%"){
+        res = parseInt(x)%parseInt(y);
+    }
+    else{res = x/y;
+    document.getElementById("res").value += "/";
+    res = parseInt(x)/parseInt(y);
+}
+document.getElementById("res").value = res;
+v = false;
 }
 
 function operacao(a){
-
-    if(a == "+"){
-  
-    }
-    else if (a == "-"){
-
-    }
-    else if (a == "*"){
-
-    }
-    else if (a == "%"){
-
-    }
-    else{res = x/y}
-    
-
-
-
+    porta = true;
+    opera = a;
+    document.getElementById("res").value += a;
 }

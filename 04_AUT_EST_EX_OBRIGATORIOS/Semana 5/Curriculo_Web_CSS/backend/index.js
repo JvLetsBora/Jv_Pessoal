53b12a67f0884012c6c04ea12dd4089c680ad7c8
joão Vitor oliveira Rodrigues <joao.rodrigues@sou.inteli.edu.br>
2022-06-19 17:25:48 -0300

    .

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Curriculo_Web_CSS/backend/index.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 5/Curriculo_Web_CSS/backend/index.js
@@ -16,25 +16,6 @@ app.use(express.json());
 /* Definição dos endpoints */
 
 /****** CRUD ******************************************************************/
-app.get('/info', (req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Access-Control-Allow-Origin',
-	'*');
-	
-	var db = new sqlite3.Database(DBPATH);
-	var sql = 'SELECT * FROM info WHERE userId = 2';
-	db.get(sql, [], (err, row) => {
-	if (err) {
-	throw err;
-	}
-	res.write('<!DOCTYPE html> \n<meta charset="UTF-8">\n<head> \n\t<title>MEUCURRÍCULO</title><style>.linha { border-bottom: solid 1px black;}</style>\n</head> \
-	\n<body> \
-	\n\t<div id="main"> \
-	\n\t\t<h1>MEU CURRÍCULO</h1>');
-	res.write('\n\t\t<div class="linha">' + row.title + '</div> \n\t</div>');
-	res.write('\n</body> \n</html>');
-	});
-	});
 
 // Retorna todos registros (é o R do CRUD - Read)
 app.get('/acessos', (req, res) => {
@@ -77,7 +58,6 @@ app.patch('/atualizaracesso', urlencodedParser, (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
 	sql = `UPDATE info SET name = '${req.body.name}', date = ${req.body.date} WHERE id = ${req.body.id} `
-	// "UPDATE info SET name = '" + req.body.name + "' date =  WHERE id = " + req.body.id;
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
 	db.run(sql, [],  err => {
 		if (err) {
@@ -104,32 +84,6 @@ app.delete('/deletaracesso', urlencodedParser, (req, res) => {
 	db.close(); // Fecha o banco
 });
 
-//Alocação 
-
-app.get('/alocacao2', (req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
-
-	var db = new sqlite3.Database(DBPATH); // Abre o banco
-  var sql = `SELECT PROJETOS.nome, SUM(horasAlocadasProjeto) FROM alocacao
-  INNER JOIN PROJETOS ON ALOCACAO.idProject = PROJETOS.idProject
-  GROUP BY PROJETOS.idProject
-  ORDER BY PROJETOS.nome COLLATE NOCASE`;
-	db.all(sql, [],  (err, rows) => {
-		if (err) {
-		    throw err;
-		
-		}
-	console.log("sou eu aqui")
-	res.json(rows)
-	});
-	db.close(); // Fecha o banco
-});
-
-// sql = SELECT PROJETOS.nome, SUM(horasAlocadasProjeto) FROM alocacao
-// INNER JOIN PROJETOS ON ALOCACAO.idProject = PROJETOS.idProject
-// GROUP BY PROJETOS.idProject
-// ORDER BY PROJETOS.nome COLLATE NOCASE
 
 /* Inicia o servidor */
 app.listen(port, hostname, () => {

b03a64e19adb658fbcc29988b484a10ae361aa65
joão Vitor oliveira Rodrigues <joao.rodrigues@sou.inteli.edu.br>
2023-08-23 16:36:28 -0300

    ...

--- docker_ 2/code/app/backend/index.js
+++ docker_ 2/code/app/backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 require("dotenv").config();
-console.log(String(process.env.USER_))
 const jwt = require("jsonwebtoken");
 //const hostname = '127.0.0.1';
 
@@ -20,13 +19,30 @@ app.use(express.json());
 
 // Definição das rotas 
 
-app.get('/app', (req, res) => {
+app.get('/app/:username',checkToken, (req, res) => {
     // Acesse o caminho do arquivo HTML que deseja servir
     const htmlFilePath = path.join(__dirname, '../frontend', 'app.html');
 
     // Envie o arquivo HTML como resposta
     res.sendFile(htmlFilePath);
 });
+
+function checkToken(req, res, next) {
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
+  
+	if (!token) return res.status(401).json({ msg: "Acesso negado!" });
+  
+	try {
+	  const secret = process.env.SECRET;
+  
+	  jwt.verify(token, secret);
+  
+	  next();
+	} catch (err) {
+	  res.status(400).json({ msg: "O Token é inválido!" });
+	}
+  }
 
 
 // Retorna todos registros 
@@ -97,47 +113,47 @@ app.delete('/deletar', urlencodedParser, (req, res) => {
 	db.close(); // Fecha o banco
 });
 
-app.post("/auth/login", async (req, res) => {
-	const { username, password } = req.body;
-	console.log(req.body)
-	// validations
-	if (!username) {
-	  return res.status(422).json({ msg: "O username é obrigatório!" });
-	}
-  
-	if (!password) {
-	  return res.status(422).json({ msg: "A senha é obrigatória!" });
-	}
 
-  
-	// check if password match
-	const checkPassword = (username,password) =>{
-		if (username && password) {
-			return true
-		}
-		return false
-	}
-  
-	if (!checkPassword) {
-	  return res.status(422).json({ msg: "Senha inválida" });
-	}
-  
-	try {
-	  const secret = process.env.SECRET;
-	  console.log(secret)
-	
-	  const token = jwt.sign(
-		{
-		  id: "user.id",
-		},
-		secret
-	  );
-  
-	  res.status(200).json({ msg: "Autenticação realizada com sucesso!", token });
-	} catch (error) {
-	  res.status(500).json({ msg: error });
-	}
-  });
+
+
+
+
+app.post("/auth/login", async (req, res) => {
+    const { username, password } = req.body;
+
+    // Validações
+    if (!username || !password) {
+        return res.status(422).json({ msg: "O username e a senha são obrigatórios!" });
+    }
+
+    try {
+        const sql = `SELECT * FROM users WHERE username = '${username}'`;
+        const db = new sqlite3.Database(DBPATH); // Abre o banco
+
+        db.get(sql, [], (err, user) => {
+            if (err) {
+                throw err;
+            }
+
+            if (user && user.password === password) {
+                const secret = process.env.SECRET;
+                const token = jwt.sign({ id: username }, secret);
+
+                res.status(200).json({ msg: "Autenticação realizada com sucesso!", token });
+            } else {
+                res.status(401).json({ msg: "Credenciais inválidas!" });
+            }
+
+            db.close(); // Fecha o banco
+        });
+    } catch (error) {
+        res.status(500).json({ msg: "Erro interno do servidor." });
+    }
+});
+
+
+
+
 
 
 /* Inicia o servidor */

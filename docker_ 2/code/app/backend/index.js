1569f00f0c5e2ff0ffc4233972b215f2a18628ce
joão Vitor oliveira Rodrigues <joao.rodrigues@sou.inteli.edu.br>
2023-08-23 09:21:26 -0300

    ...

--- docker_ 2/code/app/backend/index.js
+++ docker_ 2/code/app/backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express'); 
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
-const process = require("process")
-const dn = require("dotenv")
+require("dotenv").config();
+console.log(String(process.env.USER_))
+const jwt = require("jsonwebtoken");
 //const hostname = '127.0.0.1';
 
 const port = process.env.PORT || 3000;
@@ -14,11 +15,19 @@ const DBPATH = 'backend/dbteste.db';
 app.use(express.static(path.join(__dirname,"../frontend")));
 app.use(express.json());
 
-dn.config()
-const db_user = process.env.USER_
-const db_password = process.env.PASSWORD_
+
+
 
 // Definição das rotas 
+
+app.get('/app', (req, res) => {
+    // Acesse o caminho do arquivo HTML que deseja servir
+    const htmlFilePath = path.join(__dirname, '../frontend', 'app.html');
+
+    // Envie o arquivo HTML como resposta
+    res.sendFile(htmlFilePath);
+});
+
 
 // Retorna todos registros 
 app.get('/all', (req, res) => {
@@ -88,10 +97,57 @@ app.delete('/deletar', urlencodedParser, (req, res) => {
 	db.close(); // Fecha o banco
 });
 
+app.post("/auth/login", async (req, res) => {
+	const { username, password } = req.body;
+	console.log(req.body)
+	// validations
+	if (!username) {
+	  return res.status(422).json({ msg: "O username é obrigatório!" });
+	}
+  
+	if (!password) {
+	  return res.status(422).json({ msg: "A senha é obrigatória!" });
+	}
 
+  
+	// check if password match
+	const checkPassword = (username,password) =>{
+		if (username && password) {
+			return true
+		}
+		return false
+	}
+  
+	if (!checkPassword) {
+	  return res.status(422).json({ msg: "Senha inválida" });
+	}
+  
+	try {
+	  const secret = process.env.SECRET;
+	  console.log(secret)
+	
+	  const token = jwt.sign(
+		{
+		  id: "user.id",
+		},
+		secret
+	  );
+  
+	  res.status(200).json({ msg: "Autenticação realizada com sucesso!", token });
+	} catch (error) {
+	  res.status(500).json({ msg: error });
+	}
+  });
 
 
 /* Inicia o servidor */
 app.listen(port, () => {
   console.log(`BD server running at port ${port}`);
 });
+
+
+
+
+
+const db_user = process.env.USER_
+const db_password = process.env.PASSWORD_

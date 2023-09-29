b93a089bdd7c4dd7fb2bfac42224965304cbb0ae
JvLetsBora <99259911+JvLetsBora@users.noreply.github.com>
2023-09-29 15:38:33 -0300

    Add files via upload

---
+++ p2/p2/backend/criar_banco.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+# Constantes
+DB_USER = "postgres"
+DB_PASSWORD = "aws123456"
+DB_HOST = "database-2.cydjxbu1izwa.us-east-1.rds.amazonaws.com"
+DB_PORT = "5432"
+DB_NAME = ""
+
+# Conexão com o banco
+con = psycopg2.connect(
+    database= DB_NAME,
+    user= DB_USER,
+    password= DB_PASSWORD,
+    host= DB_HOST,
+    port= DB_PORT
+)
+
+# Criação do cursor
+cur = con.cursor()
+
+# Roda o comando SQL
+cur.execute(
+    """DROP TABLE IF EXISTS minhas_notas ;
+
+    CREATE TABLE minhas_notas (
+        id SERIAL PRIMARY KEY,
+        titulo VARCHAR(255) NOT NULL,
+        descricao TEXT NOT null,
+        data_criacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    );"""
+)
+print("Criado")
+
+# Fecha a conexão
+con.close()

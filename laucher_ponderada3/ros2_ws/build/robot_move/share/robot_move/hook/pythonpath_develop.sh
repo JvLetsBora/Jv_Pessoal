1af6c14f4fc822cbbd3e2eeb98b0ad73176c610c
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-11-18 01:15:29 -0300

    claer all

--- laucher_ponderada3/ros2_ws/build/robot_move/share/robot_move/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/jv/Documentos/Jv_Pessoal/laucher_ponderada3/ros2_ws/build/robot_move"

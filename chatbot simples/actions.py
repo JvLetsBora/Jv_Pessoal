8072fd581ddbbeeb55363fa1a70311785377b229
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-11-21 10:08:12 -0300

    chat feito

--- chatbot simples/actions.py
+++ chatbot simples/actions.py
@@ -1,29 +1,23 @@
 point_dict = {
-    "Martelo": (0.0, 0.0, 0.0, 1.0, 0.0, 0.0),
-    "Marreta": (0.0, 1.0, 0.0, 1.0, 0.0, 0.0),
-    "Prego": (0.0, 3.0, 0.0, 1.0, 0.0, 0.0),
-    "Porca": (0.0, 0.0, 0.0, 1.0, 0.0, 0.0),
-    "Chave de Fenda": (1.0, 1.0, 1.0, 0.0, 0.0, 1.0),
-    "Alicate": (-1.0, -1.0, -1.0, 0.0, 1.0, 0.0),
-    "Chave Inglesa": (2.0, 2.0, 2.0, 0.0, 1.0, 1.0),
-    "Parafuso": (-2.0, -2.0, -2.0, 1.0, 0.0, 0.0),
-    "Serra": (3.0, 3.0, 3.0, 1.0, 1.0, 1.0),
-    "Lixa": (-3.0, -3.0, -3.0, 1.0, 1.0, 0.0),
+    "martelo": ((0.0, 2.0, 0.0, 1.0, 0.0, 0.0),"Setor A"),
+    "marreta": ((60.0, 0.0, 0.0, 1.0, 0.0, 0.0),"Setor B"),
+    "prego": ((0.0, 0.2, 0.0, 1.0, 0.0, 0.0),"Setor A"),
+    "porca": ((5.0, 0.0, 0.0, 1.0, 7.0, 0.0),"Setor D"),
+    "chave de Fenda": ((0.0, 0.0, 0.0, 1.0, 0.0, 0.0),"Setor C"),
+    "alicate": ((0.0, 0.2, 0.0, 1.0, 0.0, 0.0),"Setor A"),
+    "chave Inglesa": ((6.0, 2.0, 0.0, 1.0, 0.0, 0.0),"Setor C"),
+    "parafuso": ((0.0, 0.0, 0.0, 1.0, 0.0, 7.0),"Setor A"),
+    "serra": ((6.0, 0.2, 0.0, 1.0, 0.0, 0.0),"Setor B"),
+    "lixa": ((5.0, 0.4, 0.0, 1.0, 0.0, 7.0),"Setor D"),
 }
 
 
 
 
-
-
-
-def go_to(point):
-    if point in point_dict:
-        print(f"Indo para '{point}': {point_dict[point]}")
+def control(a):
+    if a in point_dict:
+        print(f"{(str(a)[-1]).upper()} '{a}' se enconta no {point_dict[a][1]}: {point_dict[a][0]}")
     else:
-        print("Esse ponto não está cadastrado: {point}")
-
-
-def control():
-    print("teste")
-    pass
+        print(f"Esse ponto não está cadastrado: {a}")
+   
+    
--- chatbot simples/bot.py
+++ chatbot simples/bot.py
@@ -1,87 +1,34 @@
 import re
-
-
-
-# testeA = [
-#      "Como posso atualizar meu cartão de crédito?", "Preciso mudar a forma de pagamento, o que fazer?", "Quero atualizar minhas informações de pagamento", "Método de pagamento desatualizado, como proceder para atualizar?"
-# ]
-
-
-
-# input_text = ""
-
-
-
-
-# # $(como)|$(Como) & (atualizar) & (crédito) 
-# # (forma) (pagamento) *(Preciso)
-# # (pagamento) (atualizar)
-# # $(Método) (pagamento) ^(atualizar)
-# # (mudar) (forma de pagamento) 
-
-# def solicite(a):
-#     intencoes = {
-#         r'/^(Como)/':"como",
-#         r"\b[Vv][áa](?:\spara)?\s?[oa]?\s(.+)":"mudar",
-#         r'/(d08980)/':"metodo",
-
-#     }
-#     acoes = {
-#         'como':"Vá para aba meus cartões e entre em meu cartão de crédito",
-#         'mudar':"Vá para aba meus pagamentos",
-#         'metodo de pagamento':"Vá para aba mudar forma de pagamento",
-#     }
-#     input_text = a
-#     if input_text == "sair":
-#         return False
-#    # print()
-
-#     #if acoes[(intencoes[input_text]).lower()]:
-#         print(acoes[intencoes[input_text]])
-    
-#     #else:
-#        # print("Não entendi")
-
-    
-#     return True
-
-
-
-
 import actions
 
 intent_dict = {
-        r"\b[Vv][áa](?:\spara)?\s?[oa]?\s(.+)": actions.go_to,
-        r"^(Quero|Preciso|Estou|Onde encontro|procuro)\s+[a-zA-Z\s]+$":actions.control
+        r"^(Quero|Preciso|Estou|Onde encontro|procuro)\s+[a-zA-Z\s]+\s(.+)$":actions.control
         }
 
 
-
-
-# command = input("Digite o seu comando: ")
-# for key, function in intent_dict.items():
-#     pattern = re.compile(key)
-#     point = pattern.findall(command)
-#     if point:
-#         print("Encontrei uma intenção! Computando...")
-#         function(point[0])
-#         break
-
 def solicite(valor):
+    if valor == "sair":
+        return False
     for key, function in intent_dict.items():
         pattern = re.compile(key)
         point = pattern.findall(valor)
+        
+        # match = re.match(pattern, valor)
+        # print(match)
         if point:
             print("Encontrei uma intenção! Computando...")
-            function(point[0])
-            break
-    return False
+            function((point[0][1]).lower())
+    return True      
+    
 
 
 def main():
     sair = True
+    print("Chat inicializado, para sair basta escrever sair e dar enter")
     while sair:
         sair = solicite(input("Solicite algo ?"))
+    print("Programa encerrado com sucesso!")
+
 
 if __name__ == "__main__":
     main()

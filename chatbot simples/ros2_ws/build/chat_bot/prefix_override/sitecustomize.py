f21fbca95dc7268628d1e37b761f47219d306338
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-11-22 13:17:03 -0300

    ...

--- chatbot simples/ros2_ws/build/chat_bot/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/jv/Documentos/Jv_Pessoal/chatbot simples/ros2_ws/install/chat_bot'
--- chatbot_LLM/ros2_ws/src/chat_bot/chat_bot/bot.py
+++ chatbot_LLM/ros2_ws/src/chat_bot/chat_bot/bot.py
@@ -1,14 +1,16 @@
+from openai import OpenAI
 import re
 import dotenv
 from actions import go_to, control
 import os
 
-# dotenv_file = dotenv.find_dotenv()
-# dotenv.load_dotenv(dotenv_file)
+
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
 
 # print(os.environ["key"])  # outputs "value"
 # os.environ["key"] = "newvalue"
-# print(os.environ['key'])  # outputs 'newvalue'
+#print(os.environ['OPENAI_API_KEY'])  # outputs 'newvalue'
 
 # # Write changes to .env file.
 # dotenv.set_key(dotenv_file, "key", os.environ["key"])
@@ -52,11 +54,18 @@ def solicite(valor):
 
 
 def main():
-    sair = True
-    print("Chat inicializado, para sair basta escrever sair e dar enter")
-    while sair:
-        sair = solicite(input("Solicite algo ?"))
-    print("Programa encerrado com sucesso!")
+        
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    ]
+    )
+
+    print(completion.choices[0].message)
 
 
 if __name__ == "__main__":

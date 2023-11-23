317f7a5a0dcf9a1da1058ff68fe82b7f28dc86cc
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-11-23 11:51:57 -0300

    ...

---
+++ chatbot_LLM/chat_bot/bot.py
@@ -0,0 +1,77 @@
+from openai import OpenAI
+import re
+import dotenv
+from actions import go_to, control
+import os
+
+
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
+
+# print(os.environ["key"])  # outputs "value"
+# os.environ["key"] = "newvalue"
+#print(os.environ['OPENAI_API_KEY'])  # outputs 'newvalue'
+
+# # Write changes to .env file.
+# dotenv.set_key(dotenv_file, "key", os.environ["key"])
+
+
+content = ""
+
+
+
+
+intent_dict = {
+        r"\b^([Qq]uero|[Pp]reciso|[Ee]stou|[Oo]nde encontro|[Pp]rocuro)\s+[a-zA-Z\s]+\s(.+)$":control,
+        r"\b[Vv][áa](?:\spara)?\s?[oa]?\s(.+)":go_to,
+        r"\b[Mm]e\sleve até\s?[oa]?\s(.+)":go_to
+        }
+
+
+def solicite(valor):
+    if valor == "sair":
+        return False
+    for key, function in intent_dict.items():
+        pattern = re.compile(key)
+        point = pattern.findall(valor)
+        # match = re.match(pattern, valor)
+        # print(match)
+        if point:
+            print("Encontrei uma intenção! Computando...")
+            if function is go_to:
+                function((point[0]).lower())
+                break
+
+            else:
+                function((point[0][1]).lower())
+                break
+        else:
+            if r"\b[Mm]e\sleve até\s?[oa]?\s(.+)" == key:
+                print("Comando não reconhecido")
+
+    return True      
+    
+
+
+def main():
+        
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "Considere que você é um técnico de almoxarifado especializado em peças mecânicas e funcionamento fabril"},
+        {"role": "user", "content": f"Preciso que para cada problema relacionado ao mundo fabril você me forneça uma solução de que peça estou precisando para solucionar de forma breve, curta e em todos as suas respostas a palavra 'essa peça' deve estar antes da peça que você me recomendar. {content}"}
+    ]
+    )
+
+    print(completion.choices[0].message)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
--- chatbot_LLM/ros2_ws/build/chat_bot/share/chat_bot/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/jv/Documentos/Jv_Pessoal/chatbot simples/ros2_ws/build/chat_bot"

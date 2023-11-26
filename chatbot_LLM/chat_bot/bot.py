1c59176bb4c027b1474b7c8e930ab7e7cc84ceba
JvLetsBora <Joao.Rodrigues@sou.inteli.edu.br>
2023-11-26 18:55:05 -0300

    Integracao_gradio_chain

--- chatbot_LLM/chat_bot/bot.py
+++ chatbot_LLM/chat_bot/bot.py
@@ -1,78 +1,12 @@
-from openai import OpenAI
-import re
-import dotenv
-from actions import go_to, control
-import os
+import gradio as gr
+from  chain import chat_bot
 
 
-dotenv_file = dotenv.find_dotenv()
-dotenv.load_dotenv(dotenv_file)
+def chat_response(message, history):
+    return chat_bot(message)
 
-# print(os.environ["key"])  # outputs "value"
-# os.environ["key"] = "newvalue"
-#print(os.environ['OPENAI_API_KEY'])  # outputs 'newvalue'
-
-# # Write changes to .env file.
-# dotenv.set_key(dotenv_file, "key", os.environ["key"])
-
-
-content = ""
-
-
-
-
-intent_dict = {
-        r"\b^([Qq]uero|[Pp]reciso|[Ee]stou|[Oo]nde encontro|[Pp]rocuro)\s+[a-zA-Z\s]+\s(.+)$":control,
-        r"\b[Vv][áa](?:\spara)?\s?[oa]?\s(.+)":go_to,
-        r"\b[Mm]e\sleve até\s?[oa]?\s(.+)":go_to
-        }
-
-
-def solicite(valor):
-    if valor == "sair":
-        return False
-    for key, function in intent_dict.items():
-        pattern = re.compile(key)
-        point = pattern.findall(valor)
-        # match = re.match(pattern, valor)
-        # print(match)
-        if point:
-            print("Encontrei uma intenção! Computando...")
-            if function is go_to:
-                function((point[0]).lower())
-                break
-
-            else:
-                function((point[0][1]).lower())
-                break
-        else:
-            if r"\b[Mm]e\sleve até\s?[oa]?\s(.+)" == key:
-                print("Comando não reconhecido")
-
-    return True      
-    
-
-
-def main():
-        
-    client = OpenAI()
-
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "Considere que você é um técnico de almoxarifado especializado em peças mecânicas e funcionamento fabril"},
-        {"role": "user", "content": f"Preciso que para cada problema relacionado ao mundo fabril você me forneça uma solução de que peça estou precisando para solucionar de forma breve, curta e em todos as suas respostas a palavra 'essa peça' deve estar antes da peça que você me recomendar. {content}"}
-    ]
-    )
-
-    print(completion.choices[0].message)
+demo = gr.ChatInterface(chat_response)
 
 
 if __name__ == "__main__":
-    main()
-
-# python OneDrive\Documentos\GitHub\Jv_Pessoal\chatbot_LLM\chat_bot\bot.py
-
-
-
-
+    demo.launch()
--- chatbot_LLM/chat_bot/chain.py
+++ chatbot_LLM/chat_bot/chain.py
@@ -1,19 +1,33 @@
-import os
+from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
-from dotenv import load_dotenv
+from langchain.prompts.chat import ChatPromptTemplate
+import os
+import dotenv
+from typing import List
+from langchain.schema import BaseOutputParser
 
-load_dotenv()
 
-chat = ChatOpenAI()
 
-messages = [
-    SystemMessage(
-        content="Você é com um sistema especializado em fornecer informações concisas e precisas sobre normas de segurança em ambientes industriais. Você foi treinado para oferecer orientações relacionadas a equipamentos de proteção individual (EPIs), práticas seguras de operação e medidas de prevenção em diversos cenários industriais."
-    ),
-    HumanMessage(
-        content="Quais EPIs são necessários para operar um torno mecânico?"
-    ),
-]
 
-print(chat(messages).content)
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
+llm = ChatOpenAI(openai_api_key=os.environ['OPENAI_API_KEY'])
+chat_model = ChatOpenAI()
+
+class CommaSeparatedListOutputParser(BaseOutputParser[List[str]]):
+
+
+    def parse(self, text: str) -> List[str]:
+        return text
+
+template = """ Você é com um sistema especializado em fornecer informações concisas e precisas sobre normas de segurança em ambientes industriais. Você foi treinado para oferecer orientações relacionadas a equipamentos de proteção individual (EPIs), práticas seguras de operação e medidas de prevenção em diversos cenários industriais. """
+human_template = "{text}"
+
+chat_prompt = ChatPromptTemplate.from_messages([
+    ("system", template),
+    ("human", human_template),
+])
+chain = chat_prompt | ChatOpenAI() | CommaSeparatedListOutputParser()
+
+def chat_bot(msg):
+    return chain.invoke({"text": f"{msg}"})

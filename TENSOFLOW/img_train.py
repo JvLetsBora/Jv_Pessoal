3839a6d89b1e212bf897c82e8474f0c7eea7f137
JvLetsBora <joao.rodrigues@sou.inteli.edu.br>
2023-12-15 13:53:41 -0300

    u

--- TENSOFLOW/img_train.py
+++ TENSOFLOW/img_train.py
@@ -59,3 +59,41 @@ model.summary()
 
 #sudo apt install graphviz  # For Ubuntu
 #pip install pydot
+
+
+#Generate the model graph
+tf.keras.utils.plot_model(model, show_shapes=True)
+
+
+#Compile the model
+model.compile(optimizer='adam',
+			loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+			metrics=['accuracy'])
+
+
+batch_size = 128
+num_epochs = 10
+# To process the dataset in batches create the batches of batch_size
+train_dataset = train_dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+test_dataset = test_dataset.batch(batch_size)
+
+#Train the model
+model.fit(train_dataset, epochs=num_epochs, validation_data=test_dataset)
+
+
+# Step 7: Evaluate the model
+loss, accuracy = model.evaluate(test_dataset)
+print("Test loss:", loss)
+print("Test accuracy:", accuracy)
+
+
+# create a custom array of image size
+new_image = tf.constant(np.random.rand(32, 32, 3), dtype=tf.float64)
+# Extend the dimension 4D
+new_image = tf.expand_dims(new_image, axis=0)
+
+# Prediction
+predictions = model.predict(new_image)
+# predicted label
+pred_label = tf.argmax(predictions, axis =1)
+pred_label.numpy()
